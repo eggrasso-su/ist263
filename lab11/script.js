b92e3112894d0e6b6074cf9a0775863c40215245
eggrasso-su <eggrasso@syr.edu>
2024-11-07 17:15:54 -0500

    Update script.js

--- lab11/script.js
+++ lab11/script.js
@@ -1,18 +1,21 @@
-// Function to display the winning message
+
 function WinnerMessage() {
-    document.getElementById("message").innerHTML = "You Won!";
+    alert("You Won!"); 
+    document.getElementById("message").innerHTML = "You Won!"; 
 }
 
-// Function to display 'try again' message for losing shells
+
 function TryAgain() {
-    document.getElementById("message").innerHTML = "Wrong Shell - Try Again";
+    alert("Wrong Shell - Try Again"); 
+    document.getElementById("message").innerHTML = "Wrong Shell - Try Again"; 
 }
 
-// Function to shuffle the order of shells on page load
+
 function ShuffleShells() {
-    // Setting a random order for each shell image
+   
     document.getElementById("first").style.order = Math.floor(Math.random() * 3) + 1;
     document.getElementById("second").style.order = Math.floor(Math.random() * 3) + 1;
     document.getElementById("third").style.order = Math.floor(Math.random() * 3) + 1;
 }
+
 
